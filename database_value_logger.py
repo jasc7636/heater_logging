0b1bc9a14197f47780b81a86be389aad9c15d29b
jasc7636 <jasc7636@gmail.com>
2025-11-13 10:53:01 +0100

    Logging

--- database_value_logger.py
+++ database_value_logger.py
@@ -1,9 +1,13 @@
 import sqlite3
+import logging
+
+logger = logging.getLogger(__name__)
 
 class DatabaseValueLogger:
     def __init__(self, db_file: str, sensors: dict[str, dict[str, int|str]]) -> None:
         self.sensors = sensors
 
+        logger.debug(f"Connecting to database {db_file}.")
         self.connection = sqlite3.connect(db_file)
         self.cursor = self.connection.cursor()
         self._create_table()
@@ -28,12 +32,16 @@ class DatabaseValueLogger:
         self.cursor.execute("PRAGMA table_info('FROELING_SENSOR_VALUES');")
         column_names = (row[1] for row in self.cursor.fetchall())
         if not all(column in column_names for column in self.sensors.keys()):
+            logger.error("Database columns do not match sensor config.")
             raise ValueError("Database columns do not match sensor config.")
     
     def log_froeling(self, sensor_values: dict[str, float]) -> None:
+        logger.debug("Writing sensor values to database.")
+
         sensor_value_tuple = (sensor_values[sensor] for sensor in self.sensors)
         self.cursor.execute(self._sql_insert, sensor_value_tuple)
         self.connection.commit()
         
     def close(self) -> None:
+        logger.debug("Closing database connection.")
         self.connection.close()
--- froeling_modbus.py
+++ froeling_modbus.py
@@ -1,27 +1,48 @@
 import minimalmodbus
 import serial
+import logging
+
+logger = logging.getLogger(__name__)
 
 class Lambdatronic3200Modbus:
-    def __init__(self, port: str, modbus_addr: int, connection_mode: str, sensors: dict[str, dict[str, int|str]]) -> None:
+    """ Connect to a Fröling Lambdatronic 3200 via Modbus to read current values. For details on the communication protocol see https://www.holzheizer-forum.de/attachment/37208-b1200522-modbus-lambdatronic-3200-50-04-05-19-de-1-pdf/
+    """
+    
+    def __init__(self, port: str, modbus_addr: int, connection_mode: str) -> None:
         if connection_mode.lower() == "rtu":
             connection_mode = minimalmodbus.MODE_RTU
         elif connection_mode.lower() == "ascii":
             connection_mode = minimalmodbus.MODE_ASCII
         else:
+            logger.error("Invalid connection mode. Use 'rtu' or 'ascii'.")
             raise ValueError("Invalid connection mode. Use 'rtu' or 'ascii'.")
-        
+
         self.instrument = minimalmodbus.Instrument(port, modbus_addr, connection_mode)
         self.instrument.BAUDRATE = 57600
         self.instrument.PARITY = serial.PARITY_NONE
         self.instrument.STOPBITS = serial.STOPBITS_ONE
         self.instrument.BYTESIZE = serial.EIGHTBITS
         self.instrument.TIMEOUT = 1
-        self.instrument.debug = self.debug
-        
-        self.sensors = sensors
 
-    def debug_mode(self) -> None:
-        self.instrument.debug = True
+    def debug_mode(self, debug=True) -> None:
+        self.instrument.debug = debug
+        logger.debug(f"Modbus debug mode {'enabled' if debug else 'disabled'}.")
 
-    def read_sensors(self) -> dict[str, float]:
-        pass
+    def read_sensors(self, sensors: dict[str, dict[str, int|str]]) -> dict[str, float]:
+        logger.debug("Reading sensor values.")
+
+        sensor_values: dict[str, float] = {}
+        for sensor_name, sensor in sensors.items():
+            try:
+                sensor_values[sensor_name] = self.instrument.read_register(
+                    registeraddress=sensor["modbus_offset"],
+                    number_of_decimals=sensor["decimal_places"],
+                    functioncode=4,
+                    signed=True
+                )
+            except KeyError:
+                logger.error(f"Sensor '{sensor_name}' config is incomplete.")
+            except IOError:
+                logger.error(f"Failed to read sensor '{sensor_name}'.")
+
+        return sensor_values
